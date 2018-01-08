a8121cab9ca65a8244a42cccac01d5a699910829
Joshua Loyal <jloyal25@gmail.com>
2018-01-08 10:28:16 -0500

    added quadratic test

--- sir/datasets.py
+++ sir/datasets.py
@@ -25,3 +25,26 @@ def make_cubic(n_samples=500, n_features=10, n_informative=2,
 
     return X, y
 
+
+def make_quadratic(n_samples=500, n_features=10, n_informative=2,
+               random_state=None):
+    rng = check_random_state(random_state)
+
+    if n_features < n_informative:
+        raise ValueError("`n_features` must be >= `n_informative`. "
+                         "Got n_features={0} and n_informative={1}".format(
+                            n_features, n_informative))
+
+    # normally distributed features
+    X = rng.randn(n_samples, n_features)
+
+    # beta is a linear combination of informative features
+    beta = np.hstack((
+        np.ones(n_informative), np.zeros(n_features - n_informative)))
+
+    # cubic in subspace
+    y = 0.125 * np.dot(X, beta) ** 2
+    y += 0.25 * rng.randn(n_samples)
+
+    return X, y
+
--- sir/tests/test_save.py
+++ sir/tests/test_save.py
@@ -4,7 +4,7 @@ from sir import SlicedAverageVarianceEstimation
 from sir import datasets
 
 
-def test_sir():
+def test_save_cubic():
     X, y = datasets.make_cubic(random_state=123)
 
     save = SlicedAverageVarianceEstimation().fit(X, y)
@@ -12,3 +12,13 @@ def test_sir():
     true_beta = (1 / np.sqrt(2)) * np.hstack((np.ones(2), np.zeros(8)))
     angle = np.dot(true_beta, save.components_[:, 0])
     np.testing.assert_allclose(np.abs(angle), 1, rtol=1e-1)
+
+
+def test_save_quadratic():
+    X, y = datasets.make_quadratic(random_state=123)
+
+    save = SlicedAverageVarianceEstimation().fit(X, y)
+
+    true_beta = (1 / np.sqrt(2)) * np.hstack((np.ones(2), np.zeros(8)))
+    angle = np.dot(true_beta, save.components_[:, 0])
+    np.testing.assert_allclose(np.abs(angle), 1, rtol=1e-2)
