782e73e3fac94e66814f39b9f054826648208ef4
Joshua Loyal <jloyal25@gmail.com>
2018-01-18 11:55:54 -0600

    added printing of numpy and scipy versions in test script travis

--- ci_scripts/travis/test.sh
+++ ci_scripts/travis/test.sh
@@ -6,6 +6,10 @@ mkdir -p $TEST_DIR
 cp setup.cfg $TEST_DIR
 cd $TEST_DIR
 
+python --version
+python -c "import numpy; print('numpy %s' % numpy.__version__)"
+python -c "import scipy; print('scipy %s' % scipy.__version__)"
+
 if [[ "$COVERAGE" == "true" ]]; then
     pytest --cov=$MODULE --pep8 --pyargs
 else
