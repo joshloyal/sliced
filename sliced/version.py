d9d768effe386053a01cd2959be983ed67813ffa
joshloyal <jloyal25@gmail.com>
2018-01-19 18:08:29 +0000

    Pushing the docs for revision for branch: master, commit 159e64e4e27b0956a49bccf2384e7a4d43777c29

--- sliced/version.py
+++
@@ -1,9 +0,0 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
-
-__all__ = ['__version__', 'VERSION']
-
-__version__ = '0.0.1'
-VERSION = __version__
