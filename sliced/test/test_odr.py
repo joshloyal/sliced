6b87edf2c084434e298a80da48940a5d3848d3ba
Joshua Loyal <jloyal25@gmail.com>
2019-01-07 16:38:17 -0500

    added a test for odr in the cubic case

--- sliced/test/test_odr.py
+++ sliced/test/test_odr.py
@@ -12,7 +12,9 @@ from sliced import datasets
 def test_cubic():
     X, y = datasets.make_cubic(random_state=123)
 
-    odr = OuterProductGradient(n_directions=2).fit(X, y)
-    print(odr.directions_)
-
+    odr = OuterProductGradient().fit(X, y)
     odr.transform(X)
+
+    true_beta = (1 / np.sqrt(2)) * np.hstack((np.ones(2), np.zeros(8)))
+    angle = np.dot(true_beta, odr.directions_[0, :])
+    np.testing.assert_allclose(np.abs(angle), 1, rtol=1e-1)
